2360034085028f3ba9965a43b241ca55c7999fd8
eliza <elizabet002@mail.ru>
2022-04-22 16:14:16 +0300

    changed src/ast.js

--- src/ast.js
+++ src/ast.js
@@ -12,14 +12,14 @@ const getAst = (obj1, obj2) => {
         children: getAst(value1, value2),
       };
     }
-    if (!Object.hasOwn(obj1, key)) {
+    if (!_.has(obj1, key)) {
       return {
         key,
         type: 'added',
         value: value2,
       };
     }
-    if (!Object.hasOwn(obj2, key)) {
+    if (!_.has(obj2, key)) {
       return {
         key,
         type: 'removed',
